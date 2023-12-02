e3ccf2459af7407db9c464685472f371778c8954
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-02 16:26:58 +0300

    setup

---
+++ src/components/AboutMe.js
@@ -0,0 +1,27 @@
+const AboutMe = () => {
+  return (
+    <div>
+      <button>Dark Mode</button>
+      <button>Turkceye Gec</button>
+      <div>
+        <p>
+          Hi! I’m Almila. I’m a full-stack developer. I can craft solid and
+          scalable frontend products. Let’s meet!
+        </p>
+        <div>
+          <i>linkedin</i>
+          <i>github</i>
+          <p>
+            Currently Freelancing for UX, UI, & Web Design Project . Invite me
+            to join your team - pratamaiosi@gmail.com
+          </p>
+        </div>
+        <div>
+          <img src="/foto.png" alt="pp" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
