fcaf185bb21f5905598b476de4921eca5d997bd8
ozgedogru <ozgenurkarademir@gmail.com>
2023-12-04 04:28:17 +0300

    default lang:en

--- src/i18n.js
+++ src/i18n.js
@@ -4,8 +4,6 @@ import { initReactI18next } from "react-i18next";
 import tr from "./locales/tr/tr.json";
 import en from "./locales/en/en.json";
 
-const userLanguage = navigator.language.split(/[-_]/)[0];
-
 i18n.use(initReactI18next).init({
   resources: {
     tr: {
@@ -15,8 +13,8 @@ i18n.use(initReactI18next).init({
       translation: en,
     },
   },
-  lng: userLanguage,
-  fallbackLng: "en",
+  lng: "en",
+  fallbackLng: "tr",
   initImmediate: false,
 });
 
